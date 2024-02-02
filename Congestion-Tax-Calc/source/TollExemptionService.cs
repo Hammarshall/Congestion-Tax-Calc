135726a22abf88cb84cd97724eca4af2e84d7b92
wilmahammarshall <wilmahammarshall@gmail.com>
2024-02-02 15:25:47 +0100

    Created TollExemptionService.cs
    To separate the adding of Toll free vehicle

---
+++ Congestion-Tax-Calc/source/TollExemptionService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace TollFeeCalculator
+{
+    public class TollExemptionService
+    {
+        // List of toll-free vehicle types
+        private readonly List<string> _exemptVehicleTypes;
+
+        public TollExemptionService()
+        {
+            // En lista över fordonstyper som är undantagna från trängselskatt.
+            // Lätt att addera nya typer som är undantagna från trängselskatt
+            _exemptVehicleTypes = new List<string> { "Diplomat", "Foreign", "Military" };
+        }
+
+        // Determines if a vehicle is toll-free based on its type
+        public bool IsExempt(string vehicleType)
+        {
+            // Kontrollerar om det angivna fordonet är undantaget från trängselskatt.
+            return _exemptVehicleTypes.Contains(vehicleType);
+        }
+    }
+}
