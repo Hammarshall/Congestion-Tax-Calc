de9eeb72f13ce57f4ea436fdf156113607ebe6a7
wilmahammarshall <wilmahammarshall@gmail.com>
2024-02-02 15:46:05 +0100

    Created TollFeePeriod.cs
    to separate the logic for TollFeePeriods

---
+++ Congestion-Tax-Calc/source/TollFeePeriod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TollFeeCalculator
+{
+    public class TollFeePeriod
+    {
+        public TimeSpan Start { get; set; } // Starttiden för avgiftsperioden.
+        public TimeSpan End { get; set; }   // Sluttiden för avgiftsperioden.
+        public int Fee { get; set; }        // Avgiften som tillämpas under denna period.
+
+        public TollFeePeriod(TimeSpan start, TimeSpan end, int fee)
+        {
+            // Konstruktor skapar ny avgiftsperiod.
+            Start = start;
+            End = end;
+            Fee = fee;
+        }
+    }
+}
