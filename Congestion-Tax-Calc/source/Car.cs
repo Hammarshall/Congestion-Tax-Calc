ca3a261690c212838679b5e14fbff093be8f8140
wilmahammarshall <wilmahammarshall@gmail.com>
2024-02-02 15:47:44 +0100

    Update car.cs
    Updated car. cs to match with Vehicle interface

--- Congestion-Tax-Calc/source/Car.cs
+++ Congestion-Tax-Calc/source/Car.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using TollFeeCalculator;
 
 namespace TollFeeCalculator
 {
-    public class Car : Vehicle
+    public class Car : IVehicle
     {
-        public String GetVehicleType()
+        // GetVehicleType från IVehicle-gränssnittet = enhetlig behandling av fordonstyper.
+        private readonly TollExemptionService _exemptionService;
+
+        public Car(TollExemptionService exemptionService)
+        {
+            _exemptionService = exemptionService;
+        }
+
+        // Returnerar fordonstypssträng, måste implementeras av alla typer, enhetligt och förutsägbart.
+        public string GetVehicleType()
         {
             return "Car";
         }
+
+        public bool IsTollExempt()
+        {
+            return _exemptionService.IsExempt(GetVehicleType());
+        }
+
+        // Här kan ytterligare specifika metoder och egenskaper för car läggas till.
+
     }
 }
