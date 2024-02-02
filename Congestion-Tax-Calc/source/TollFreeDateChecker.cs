e114bf45d98c605344df1d1f70192bf05ec2d4d6
wilmahammarshall <wilmahammarshall@gmail.com>
2024-02-02 15:37:05 +0100

    Create TollFreeDateChecker.c
    Separated the more static toll free dates and checks the dates (same function more or less to TollFreeDates but with more static dates)

---
+++ Congestion-Tax-Calc/source/TollFreeDateChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using TollFeeCalculator;
+
+namespace TollFeeCalculator
+{
+    public class TollFreeDateChecker
+    {
+        private readonly TollFreeDates _tollFreeDates;
+
+        // List of toll-free dates (public holidays, weekends)
+        public TollFreeDateChecker(TollFreeDates tollFreeDates)
+        {
+            _tollFreeDates = tollFreeDates;
+        }
+
+        // Check specific date is toll-free (either in the toll-free dates list or on weekends)
+        public bool IsTollFreeDate(DateTime date)
+        {
+            // Implementera kontroll av helgdagar, dagar före helgdagar, lördagar och juli månad
+            return _tollFreeDates.IsTollFreeDate(date) ||
+           date.DayOfWeek == DayOfWeek.Saturday ||
+           date.DayOfWeek == DayOfWeek.Sunday ||
+           date.Month == 7;
+        }
+    }
+}
