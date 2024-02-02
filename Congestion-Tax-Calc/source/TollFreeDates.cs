9955a11f7d73ee89995e6b9f43d72ee70671ff38
wilmahammarshall <wilmahammarshall@gmail.com>
2024-02-02 15:32:26 +0100

    Created TollFreeDates.c
    To separate and handle a list of dynamic toll free dates (hollidays etc that changes every year) and checks if the date it in the list and toll free

---
+++ Congestion-Tax-Calc/source/TollFreeDates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TollFeeCalculator
+{
+    public class TollFreeDates
+    {
+        // List of specific toll-free dates
+        private readonly List<DateTime> _tollFreeDates;
+
+
+        // List of dates that are toll-free (e.g., public holidays, weekends) YYMMDD
+        public TollFreeDates()
+        {
+            _tollFreeDates = new List<DateTime>
+            {
+                // Initialize with specific toll-free dates
+                new DateTime(2024, 1, 1),
+                new DateTime(2024, 3, 28),
+                new DateTime(2024, 3, 29),
+                // ... Add more dates here ...
+            };
+        }
+
+        // Checks if a given date is toll-free
+        public bool IsTollFreeDate(DateTime date) => _tollFreeDates.Contains(date.Date) || date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday ||
+                   date.Month == 7;
+    }
+}
